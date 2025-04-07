1ee2d814db2d308eedb331bbe05a256174323bbb
rahman-a <ahm.abdrahman@gmail.com>
2025-04-07 12:03:02 +0200

    set status code to 403 on entry route of application

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 import { join } from 'path';
@@ -8,6 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/')
+  @HttpCode(403)
   entry(@Res() res: Response) {
     res.sendFile(join(__dirname, '..', 'public', 'access-denied.html'));
   }
