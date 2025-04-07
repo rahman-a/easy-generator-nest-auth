82c26a02535a1d731e39d83e0600e5d362b2ad9c
rahman-a <ahm.abdrahman@gmail.com>
2025-04-07 10:22:51 +0200

    add production frontend url to allowed origins

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
-/* eslint-disable prettier/prettier */
+import { ForbiddenException } from '@nestjs/common';
+
 export default () => ({
   database: {
     connectionString: process.env.MONGODB_URI,
@@ -11,3 +12,25 @@ export default () => ({
     production: process.env.NODE_ENV === 'production',
   },
 });
+
+export const origins =
+  process.env.NODE_ENV === 'production'
+    ? ['https://easy-generator-react-test-production.up.railway.app']
+    : ['http://localhost:5173', 'http://127.0.0.1:5173'];
+
+// console.log('env: ', process.env.NODE_ENV);
+
+export const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void,
+  ) {
+    if (!origin || origins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new ForbiddenException('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { corsOptions } from './config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(corsOptions);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
